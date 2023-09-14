90de0c96bc74234c8ec6ee698ebb534a5e6408e7
KEYS248 <kleinjdavid@gmail.com>
2023-09-14 13:30:16 -0700

    Update submit_all.sh

--- submit_all.sh
+++ submit_all.sh
@@ -28,9 +28,9 @@ Defaults: \n\t\
     blast_db_types: nt \n\t\
     num_alignments: 250 \n\t\
     align_min: 90 \n\n\
-Run after demultiplexing and fastq directory: \n\t\
+Run after demultiplexing and with fastq directory: \n\t\
     sh ${PIPELINE_DIR}/submit_all.sh --fastq_dir /oak/stanford/groups/cgawad/2020-01-01_Fastqs/ --project 2020-01-01_Project \n\n\
-Run after demultiplexing and results directory: \n\t\
+Run after demultiplexing and with results directory: \n\t\
     sh ${PIPELINE_DIR}/submit_all.sh --fastq_dir /oak/stanford/groups/cgawad/2020-01-01_Fastqs/ --results_dir /oak/stanford/groups/cgawad/2020-01-01_Results/ --project 2020-01-01_Project \n\n\
 Run with demultiplexing and fastq directory: \n\t\
     sh ${PIPELINE_DIR}/submit_all.sh --run_dir /oak/stanford/groups/cgawad/Illumina_Data/MiniSeq/2020-01-01_BCLs --fastq_dir /oak/stanford/groups/cgawad/2020-01-01_Fastqs/ --project 2020-01-01_Project \n\n\

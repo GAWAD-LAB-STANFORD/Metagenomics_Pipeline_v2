ff855f4fcb7a526f106569833f810d356f102c04
David Klein <kleinjdavid@gmail.com>
2023-09-18 18:29:08 -0700

    rsync typo

--- submit_all.sh
+++ submit_all.sh
@@ -567,7 +567,7 @@ elif [ $STEP -eq 3 ]; then
     
     if [ "$SCRATCH_DIR" != "$RESULTS_DIR" ]; then
         echo "### Moving results from scratch dir to results dir ### - START: $(date)"
-        rsync -ar $SCRATCH_DIR $RESULTS_DIRrsync -ar
+        rsync -ar $SCRATCH_DIR $RESULTS_DIR
         echo "### Moving results from scratch dir to results dir ### - END: $(date)"
     fi
     echo "END: $(date)" >> $PIPELINE_STATUS

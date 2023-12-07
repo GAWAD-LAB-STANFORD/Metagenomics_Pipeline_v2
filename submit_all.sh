3a7cdf826a267b9ca8567c32983dd00680bf417b
David Klein <kleinjdavid@gmail.com>
2023-12-07 14:14:50 -0800

    jobs to run

--- submit_all.sh
+++ submit_all.sh
@@ -385,7 +385,7 @@ elif ([ $STEP -eq 0 ] && [ -z $RUN_DIR ] && [ $ONLY_IDENTIFY -eq 0 ]) || ([ $STE
         echo -e "Number of samples: ${#SAMPLE_ARRAY[@]}\nSamples: ${SAMPLE_ARRAY[@]}\n" >> $PIPELINE_STATUS
         echo "### Processing fastq samples ### - START: $(date)" >> $PIPELINE_STATUS
         JOB_COUNT=${#SAMPLE_ARRAY[@]}
-        echo "Process sample jobs to run: $JOB_COUNT" >> $PIPELINE_STATUS
+        echo "Jobs to run: $JOB_COUNT" >> $PIPELINE_STATUS
         TEMP_ARRAY_START=1
     fi
     
@@ -485,7 +485,7 @@ if ([ $STEP -eq 0 ] && [ $ONLY_IDENTIFY -eq 1 ]) || [ $STEP -eq 2 ]; then
         echo -e "Number of samples: ${#SAMPLE_ARRAY[@]}\nSamples: ${SAMPLE_ARRAY[@]}" >> $PIPELINE_STATUS
         echo "### Processing ref filtered samples ### - START: $(date)" >> $PIPELINE_STATUS
         JOB_COUNT=${#SAMPLE_ARRAY[@]}
-        echo "Process sample jobs to run: $JOB_COUNT" >> $PIPELINE_STATUS
+        echo "Jobs to run: $JOB_COUNT" >> $PIPELINE_STATUS
         TEMP_ARRAY_START=1
     fi
         
